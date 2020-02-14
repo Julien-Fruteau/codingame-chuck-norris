a86d5a61b7c9f8a68c2903d91b1173488f60c2d8
Julien-Fruteau <julien.fruteau@gmail.com>
2020-02-14 14:23:51 +0100

    done unaire encoding of unaire seq ('111' or '000')

--- __main__.py
+++ __main__.py
@@ -6,50 +6,45 @@ import math
 
 message = input()
 
-chr(99)
-bin(67)
-
-# for each letter in string
-# get the ASCII code of the charater
-# get the bin code of the ASCII code
+# get the bin code of the ASCII code of each letter in message
+# 'h' = '11010000'
+# for each uniq seq in binEncoded(Char), chuckEncode(Seq)
 # 001011100 => 00 1 0 111 00
 #              00 00 0 0 0 000 00 00
 
-encodeSeq(000)
 
-
-# generateur
-def generateur(l, o, i=-1):
-    while True:
-        try:
-            i = l.index(o, i+1)
-            yield i
-        except ValueError:
-            return
-        
-
-splitInput("1001")
-for c in splitInput("1001"):
-    print(c)
-
-def splitInput(message):
+def binEncodedRange(message):
     for charac in message:
-        chrToStr = bin(ord(charac))[2:]
-        yield chrToStr
+        chrToBinStr = bin(ord(charac))[2:]
+        yield chrToBinStr
 
-def splitCharTo(seq):
 
+# result = [ " ".join(encodeBinSeq()) ]
+for binEnc in binEncodedRange(message):
+    for unaireSeq in unaireSeqRange(binEnc):
+        unaireEncode(unaireSeq)
+
+
+
+@unaireSeqValidation
+def unaireSeqRange(binEncodedChar):
     pass
 
 
-@checkSeq
-def encodeSeq(seq):
-    result = '0 ' if seq[0] == '1' else '00 '
-    result += '0' * len(seq)
+def unaireSeqValidation(f):
+    def wrapped(seq):
+        r = f(seq)
+    return wrapped
+
+
+@seqValidation
+def unaireEncode(unaireSeq):
+    result = '0 ' if unaireSeq[0] == '1' else '00 '
+    result += '0' * len(unaireSeq)
     return result
 
 
-def checkSeq(toWrap):
+def seqValidation(f):
     def getCharCount(seq):
         return {i: seq.count(i) for i in ['0', '1']}
 
@@ -62,35 +57,10 @@ def checkSeq(toWrap):
         if not hasUniqueValue(seq):
             raise ValueError(
                 "Only '0' OR '1' are accepted in sequence {}".format(seq))
-        r = toWrap(seq)
+        r = f(seq)
         return r
     return wrapped
 
-
-encodeSeq('10111')
-seq = '000'
-
-
-def getCharCount(seq):
-    return {i: seq.count(i) for i in ['0', '1']}
-
-
-for value in :
-    print(value)
-
-0 in getCharCount(seq).values()
-
-bin(99)
-bin(int.from_bytes("a".encode(),'big'))
-"a".encode()
-str(11101)
-
-
-def bloc1(charSeq):
-    what = "has1" if "1" in charSeq else "has0"
-    res = {"has1": "0",
-           "has0": "00"}
-    return res[what]
 
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr)
